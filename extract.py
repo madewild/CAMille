7fe4616071c2337b20598ca44c10fb79024fc1ea
madewild <madewild@ulb.ac.be>
2020-10-22 10:52:29 +0200

    utf8 by default for xml

--- extract.py
+++ extract.py
@@ -7,7 +7,7 @@ data_path = "data/xml/"
 files = os.listdir(data_path)
 
 for f in files:
-    lines = open(data_path+f).readlines()
+    lines = open(data_path+f, encoding="utf-8").readlines()
     out_path = f"data/txt/{f[:-4]}.txt"
     output = open(out_path, "w", encoding="utf-8")
     words = []
