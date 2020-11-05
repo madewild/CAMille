99b848f3f04b6f31af755030d28120194b807531
madewild <madewild@ulb.ac.be>
2020-11-05 22:58:26 +0100

    add xml parsing script

---
+++ parse.py
@@ -0,0 +1,25 @@
+"""Parse XML files and retrieve formatted text"""
+
+import os
+import re
+from bs4 import BeautifulSoup as bs
+
+data_path = "data/xml/"
+files = os.listdir(data_path)
+
+for f in files:
+    xml_string = open(data_path+f, encoding="utf-8").read()
+    soup = bs(xml_string, "lxml")
+    out_path = f"data/txt/{f[:-4]}.txt"
+    output = open(out_path, "w", encoding="utf-8")
+    extracted_text = ""
+    lines = soup.find_all("textline")
+    for line in lines:
+        words = []
+        strings = line.find_all("string")
+        for string in strings:
+            word = string.get("content")
+            words.append(word)
+        extracted_line = " ".join(words) + "\n"
+        extracted_text += extracted_line
+    output.write(extracted_text)
