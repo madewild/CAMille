beafd10ba8790550f6ee321b8661c995c93a90ae
madewild <max.de.wilde@ulb.be>
2021-01-28 10:15:30 +0100

    better html

--- __init__.py
+++ __init__.py
@@ -4,6 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "<h1>CAMILLE</h1><h2>Centre d'Archives sur les Médias et l'Information"
+    html = """<h1>CAMILLE</h1>
+              <h2>Centre d'Archives sur les Médias et l'Information</h2>
+           """
+    return html
 if __name__ == "__main__":
     app.run()
