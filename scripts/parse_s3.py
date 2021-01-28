ab7dd05554617e8e4accd1d7f969389d056049b3
madewild <max.de.wilde@ulb.be>
2021-01-28 13:00:45 +0100

    skip year if not found

--- scripts/parse_s3.py
+++ scripts/parse_s3.py
@@ -25,40 +25,45 @@ username = cred["username"]
 password = cred["password"]
 headers = {"Content-Type": "application/json; charset=utf8"}
 
-for obj in s3.list_objects(Bucket=bucket_name, Prefix=prefix)["Contents"]:
-    key = obj["Key"]
-    file_name = key.split("/")[-1]
-    raw_file_name = file_name[:-4]
-    date = raw_file_name.split("_")[2]
-    print(f"Processing {file_name}")
-    xml = s3r.Object(bucket_name, key)
-    body = xml.get()['Body'].read()
-    soup = bs(body, "lxml")
-    extracted_lines = []
-    lines = soup.find_all("textline")
-    for line in lines:
-        words = []
-        strings = line.find_all("string")
-        for string in strings:
-            subs_type = string.get("subs_type")
-            if subs_type:
-                if subs_type == "HypPart1":
-                    word = string.get("subs_content")
-                elif subs_type == "HypPart2":
-                    word = None
+obj_list = s3.list_objects(Bucket=bucket_name, Prefix=prefix)
+try:
+    objects = obj_list["Contents"]
+    for obj in objects:
+        key = obj["Key"]
+        file_name = key.split("/")[-1]
+        raw_file_name = file_name[:-4]
+        date = raw_file_name.split("_")[2]
+        print(f"Processing {file_name}")
+        xml = s3r.Object(bucket_name, key)
+        body = xml.get()['Body'].read()
+        soup = bs(body, "lxml")
+        extracted_lines = []
+        lines = soup.find_all("textline")
+        for line in lines:
+            words = []
+            strings = line.find_all("string")
+            for string in strings:
+                subs_type = string.get("subs_type")
+                if subs_type:
+                    if subs_type == "HypPart1":
+                        word = string.get("subs_content")
+                    elif subs_type == "HypPart2":
+                        word = None
+                    else:
+                        print(f"Unknown SUBS_TYPE: {subs_type}")
+                        sys.exit()
                 else:
-                    print(f"Unknown SUBS_TYPE: {subs_type}")
-                    sys.exit()
-            else:
-                word = string.get("content")
-            if word:
-                words.append(html.unescape(word))
-        extracted_line = " ".join(words)
-        extracted_lines.append(extracted_line)
-    extracted_text = " ".join(extracted_lines)
-    payload = {"page": raw_file_name, "date": date, "text": extracted_text}
-    data = json.dumps(payload)
-    r = requests.post(es_url, auth=(username, password), headers=headers, data=data)
-    if r.status_code != 201:
-        print(r.status_code)
-        sys.exit()
+                    word = string.get("content")
+                if word:
+                    words.append(html.unescape(word))
+            extracted_line = " ".join(words)
+            extracted_lines.append(extracted_line)
+        extracted_text = " ".join(extracted_lines)
+        payload = {"page": raw_file_name, "date": date, "text": extracted_text}
+        data = json.dumps(payload)
+        r = requests.post(es_url, auth=(username, password), headers=headers, data=data)
+        if r.status_code != 201:
+            print(r.status_code)
+            sys.exit()
+except KeyError:
+    print(f"{year} has not been found, skipping")
