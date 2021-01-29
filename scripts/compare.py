960bc73dfdc6703fbf45272644775c635dce58a0
madewild <max.de.wilde@ulb.be>
2021-01-29 09:06:41 +0100

    walk all folders and write to file

--- scripts/compare.py
+++ scripts/compare.py
@@ -2,12 +2,13 @@
 
 import os
 
-path_xml = "/run/media/max/Backup Plus/BelgicaPress XML/JB427/"
-path_pdf = "/run/media/max/Backup Plus/BelgicaPress PDF/JB427/"
+path_xml = "/run/media/max/Backup Plus/BelgicaPress XML/"
+path_pdf = "/run/media/max/Backup Plus/BelgicaPress PDF/"
 
 xml_files = [f.split("/")[-1][:-4] for f in [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(path_xml)) for f in fn]]
 pdf_files = [f.split("/")[-1][:-4] for f in [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(path_pdf)) for f in fn]]
 
+output = open("data/missing_pdf.txt", "w")
 for f in xml_files:
     if f not in pdf_files:
-        print(f)
+        output.write(f + "\n")
