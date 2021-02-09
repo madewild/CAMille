54b7bbac0821e3549956d96a96f05297f0854f1e
madewild <max.de.wilde@ulb.be>
2021-02-09 07:38:03 +0100

    iterate over all years

--- scripts/check.py
+++ scripts/check.py
@@ -1,6 +1,7 @@
 """Find missing texts in ES"""
 
 import json
+import os
 import sys
 
 import boto3
@@ -13,43 +14,48 @@ paginator = s3.get_paginator('list_objects')
 
 bucket_name = "camille-data"
 code = sys.argv[1]
-year = sys.argv[2]
+try:
+    years = [sys.argv[2]]
+except IndexError:
+    #years = os.listdir(f"/run/media/max/Backup plus/Belgica Press XML/{code}/")
+    years = range(1910, 1970)
 
 cred = json.load(open("../credentials.json"))
 endpoint = cred["endpoint"]
-es_url = f"{endpoint}/pages/_search?track_total_hits=true&q=journal:{code}%20AND%20year:{year}"
 username = cred["username"]
 password = cred["password"]
 headers = {"Content-Type": "application/json; charset=utf8"}
-
 payload = {
             "size": 10000,
             "stored_fields": []
 }
 
-resp = requests.request("POST", es_url, auth=(username, password), data=json.dumps(payload), headers=headers)
-resp = json.loads(resp.text)
-es_ids = set([hit["_id"] for hit in resp["hits"]["hits"]])
-nb_es_ids = len(es_ids)
-print(f"{nb_es_ids} docs found for journal {code} and year {year}")
+for year in years:
+    es_url = f"{endpoint}/pages/_search?track_total_hits=true&q=journal:{code}%20AND%20year:{year}"
+    resp = requests.request("POST", es_url, auth=(username, password), data=json.dumps(payload), headers=headers)
+    resp = json.loads(resp.text)
+    es_ids = set([hit["_id"] for hit in resp["hits"]["hits"]])
+    nb_es_ids = len(es_ids)
+    print(f"{nb_es_ids} docs found for journal {code} and year {year}")
 
-prefix = f"XML/{code}/{year}"
-pages = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
-for page in pages:
-    objects = page["Contents"]
-    for obj in objects:
-        key = obj["Key"]
-        file_name = key.split("/")[-1]
-        raw_file_name = file_name[:-4]
-        if raw_file_name not in es_ids:
-            print(f"{raw_file_name} is missing")
-            write_to_es(s3, bucket_name, key, cred)
+    if nb_es_ids:
+        prefix = f"XML/{code}/{year}"
+        pages = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
+        for page in pages:
+            objects = page["Contents"]
+            for obj in objects:
+                key = obj["Key"]
+                file_name = key.split("/")[-1]
+                raw_file_name = file_name[:-4]
+                if raw_file_name not in es_ids:
+                    print(f"{raw_file_name} is missing")
+                    write_to_es(s3, bucket_name, key, cred)
 
-resp = requests.request("POST", es_url, auth=(username, password), data=json.dumps(payload), headers=headers)
-resp = json.loads(resp.text)
-new_es_ids = set([hit["_id"] for hit in resp["hits"]["hits"]])
-nb_new_es_ids = len(new_es_ids)
-if nb_new_es_ids > nb_es_ids:
-    print(f"Now {nb_new_es_ids} docs found for journal {code} and year {year}")
-else:
-    print("Nothing missing")
+        resp = requests.request("POST", es_url, auth=(username, password), data=json.dumps(payload), headers=headers)
+        resp = json.loads(resp.text)
+        new_es_ids = set([hit["_id"] for hit in resp["hits"]["hits"]])
+        nb_new_es_ids = len(new_es_ids)
+        if nb_new_es_ids > nb_es_ids:
+            print(f"Now {nb_new_es_ids} docs for journal {code} and year {year}")
+        else:
+            print("Nothing missing")
