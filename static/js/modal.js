2ba9ea47b12a420c7c7e84c705a195bf55d11c08
madewild <max.de.wilde@ulb.be>
2021-05-06 08:13:02 +0200

    hide overflow when modal open

--- static/js/modal.js
+++ static/js/modal.js
@@ -10,16 +10,22 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
+  document.body.style.overflow = "hidden";
+  document.body.style.height = "100%";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  document.body.style.overflow = "auto";
+  document.body.style.height = "auto";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    document.body.style.overflow = "auto";
+    document.body.style.height = "auto"; 
   }
 }
