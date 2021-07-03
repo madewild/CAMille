a9231614b2aaf2442047d4e3449de3f4650d6e48
madewild <max.de.wilde@ulb.be>
2021-07-03 09:28:36 +0200

    temporary strikethrough fix

--- __init__.py
+++ __init__.py
@@ -156,7 +156,7 @@ def hello():
                     matches = hit["highlight"]["text"]
                 except KeyError: # no matches (wildcard), defaulting to 500 first chars
                     matches = [hit["_source"]["text"][:500] + "..."]
-                all_matches = " [...] ".join(matches).replace("<", "")
+                all_matches = " [...] ".join(matches).replace("<S", "")
                 result = {"id": result_id, "display": display, "matches": all_matches}
                 results.append(result)
 
