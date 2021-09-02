fa211032806fc8b332946fe03e7e3623e5f34c51
madewild <max.de.wilde@ulb.be>
2021-09-02 23:11:24 +0200

    remove duplicated function

--- static/js/slider_page.js
+++ static/js/slider_page.js
@@ -26,13 +26,3 @@ $( function() {
   $("#page_range").val("De la page " + $("#slider-range-page").slider("values", 0) +
     " à la page " + $("#slider-range-page").slider("values", 1));
 } );
-
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-      results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-  }
