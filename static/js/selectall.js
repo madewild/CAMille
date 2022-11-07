d3c7fd4e07593d033e26f7fd44f7bff7df408098
madewild <max.de.wilde@ulb.be>
2022-11-07 23:02:53 +0100

    extend selectall to months and dows

--- static/js/selectall.js
+++ static/js/selectall.js
@@ -1,9 +1,29 @@
 $(document).ready(function () {
-    $('body').on('click', '#selectAll', function () {
+    $('body').on('click', '#selectAllPapers', function () {
       if ($(this).hasClass('allChecked')) {
-          $('input[type="checkbox"]', '#selectall').prop('checked', false);
+          $('input[type="checkbox"]', '#selectallpapers').prop('checked', false);
       } else {
-          $('input[type="checkbox"]', '#selectall').prop('checked', true);
+          $('input[type="checkbox"]', '#selectallpapers').prop('checked', true);
+      }
+      $(this).toggleClass('allChecked');
+    })
+  });
+$(document).ready(function () {
+    $('body').on('click', '#selectAllMonths', function () {
+      if ($(this).hasClass('allChecked')) {
+          $('input[type="checkbox"]', '#selectallmonths').prop('checked', false);
+      } else {
+          $('input[type="checkbox"]', '#selectallmonths').prop('checked', true);
+      }
+      $(this).toggleClass('allChecked');
+    })
+  });
+$(document).ready(function () {
+    $('body').on('click', '#selectAllDays', function () {
+      if ($(this).hasClass('allChecked')) {
+          $('input[type="checkbox"]', '#selectalldays').prop('checked', false);
+      } else {
+          $('input[type="checkbox"]', '#selectalldays').prop('checked', true);
       }
       $(this).toggleClass('allChecked');
     })
