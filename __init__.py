e2a251ddd3d88e76d58a454bc9b832f43f6d5a05
madewild <max.de.wilde@ulb.be>
2022-12-12 10:20:56 +0100

    keep basic auth for prod

--- __init__.py
+++ __init__.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 import json
 import locale
 import math
+import os
 from pathlib import Path
 from shutil import copy
 from unidecode import unidecode
@@ -13,7 +14,6 @@ from zipfile import ZipFile
 import boto3
 
 from flask import Flask, request, render_template, send_file
-from flask_htpasswd import HtPasswdAuth
 
 import pandas as pd
 import requests
@@ -26,12 +26,14 @@ except FileNotFoundError:
 locale.setlocale(locale.LC_ALL, 'fr_BE.utf8')
 
 app = Flask(__name__)
-
-#app.config['FLASK_HTPASSWD_PATH'] = '/etc/apache2/.htpasswd'
-#app.config['FLASK_AUTH_ALL'] = True
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-#htpasswd = HtPasswdAuth(app)
+workdir =  os.getcwd()
+if workdir.endswith("prod"):
+    from flask_htpasswd import HtPasswdAuth
+    app.config['FLASK_HTPASSWD_PATH'] = '/etc/apache2/.htpasswd'
+    app.config['FLASK_AUTH_ALL'] = True
+    htpasswd = HtPasswdAuth(app)
 
 @app.template_filter()
 def strip_param(long_url, param):
