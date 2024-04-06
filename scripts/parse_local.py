64101d00e35c68d0a0e84d57e105bc5bba85f76a
madewild <max.de.wilde@ulb.be>
2024-04-06 12:33:43 +0200

    generalise to periodicals

--- scripts/parse_local.py
+++ scripts/parse_local.py
@@ -11,15 +11,15 @@ data_path = f"/run/media/max/CAMille 1/{journal}/"
 dirs = os.listdir(data_path)
 
 for dir in sorted(dirs):
-    print(f"Year {dir}")
+    print(f"Folder {dir}")
     files = os.listdir(data_path+dir)
     for f in sorted(files):
-        #print(f"Processing {f}")
-        xml_string = open(data_path+dir+"/"+f, encoding="utf-8").read()
-        out_path = f"/run/media/max/CAMille 1/{journal}_TXT/{dir}/"
-        if not os.path.exists(out_path):
-            os.makedirs(out_path)
-        full_out_path = f"{out_path}{f[:-4]}.txt"
-        output = open(full_out_path, "w", encoding="utf-8")
-        extracted_text = extract_text(xml_string)
-        output.write(extracted_text)
+        if f.endswith(".xml"):
+            xml_string = open(data_path+dir+"/"+f, encoding="utf-8").read()
+            out_path = f"/run/media/max/CAMille 1/{journal}_TXT/{dir}/"
+            if not os.path.exists(out_path):
+                os.makedirs(out_path)
+            full_out_path = f"{out_path}{f[:-4]}.txt"
+            output = open(full_out_path, "w", encoding="utf-8")
+            extracted_text = extract_text(xml_string)
+            output.write(extracted_text)
