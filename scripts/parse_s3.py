6455e1bda632a9ab31e35a743436c824f7e3ae65
madewild <max.de.wilde@ulb.be>
2024-04-17 22:06:31 +0200

    align with lambda

--- scripts/parse_s3.py
+++ scripts/parse_s3.py
@@ -1,5 +1,6 @@
 """Parse XML files from S3 and retrieve text"""
 
+import datetime
 import html
 import json
 import sys
@@ -16,6 +17,7 @@ def requests_retry_session(
     status_forcelist=(500, 502, 503, 504),
     session=None,
 ):
+    """Retry upon failure"""
     session = session or requests.Session()
     retry = Retry(
         total=retries,
@@ -64,11 +66,46 @@ def write_to_es(client, bucket_name, key, cred):
     elements = raw_file_name.split("_")
     journal = elements[1]
     date = elements[2]
-    docyear = date.split("-")[0]
+    if elements[0] == "BE-KBR00": # new KBR format
+        if journal == "15463334": # La Presse
+            journal = "B14138"
+        else: # unknown journal
+            sys.exit()
+        edition = elements[3]
+        pagenb = "0" + elements[8] # add leading zero
+        date = date[:4] + "-" + date[4:6] + "-" + date[6:8]
+        date_format = "%Y%m%d"
+    else:
+        ed_page = elements[3]
+        ep_elems = ed_page.split("-")
+        edition = ep_elems[0]
+        pagenb = ep_elems[1]
+        date_format = "%Y-%m-%d"
+    date_elems = date.split("-")
+    docyear = date_elems[0]
+    month = date_elems[1]
+    day = date_elems[2]
+    ts = datetime.datetime.strptime(date, date_format)
+    dow = str(ts.weekday() + 1)
+
     new_obj = client.get_object(Bucket=bucket_name, Key=key)
     body = new_obj['Body'].read()
     text = extract_text(body)
-    payload = {"page": raw_file_name, "journal": journal, "year": docyear, "date": date, "text": text}
+
+    payload = {
+            "page": raw_file_name, 
+            "journal": journal,
+            "date": date,
+            "year": docyear, 
+            "month": month,
+            "day": day,
+            "dow": dow,
+            "edition": edition,
+            "pagenb": pagenb,
+            "language": "fr-BE",
+            "text": text
+        }
+
     data = json.dumps(payload)
     endpoint = cred["endpoint"]
     es_url = f"{endpoint}/pages/_doc"
@@ -96,7 +133,7 @@ if __name__ == "__main__":
     s3 = boto3.client('s3')
     paginator = s3.get_paginator('list_objects')
 
-    bucket_name = "camille-data"
+    BUCKET = "camille-data"
     code = sys.argv[1]
     start = int(sys.argv[2])
     try:
@@ -106,22 +143,22 @@ if __name__ == "__main__":
     years = range(start, end+1)
 
     try:
-        credentials = json.load(open("../credentials.json"))
+        credentials = json.load(open("../credentials.json", encoding="utf-8"))
     except FileNotFoundError:
-        credentials = json.load(open("/var/www/camille/credentials.json"))
+        credentials = json.load(open("/var/www/camille/credentials.json", encoding="utf-8"))
 
     for year in years:
         print(f"Processing {year}...")
         prefix = f"XML/{code}/{year}"
         #prefix = f"XML/{code}/{year}/KB_{code}_{year}-1"
 
-        pages = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
+        pages = paginator.paginate(Bucket=BUCKET, Prefix=prefix)
         for page in pages:
             try:
                 objects = page["Contents"]
                 for obj in objects:
-                    key = obj["Key"]
-                    write_to_es(s3, bucket_name, key, credentials)
+                    KEY = obj["Key"]
+                    write_to_es(s3, BUCKET, KEY, credentials)
 
             except KeyError:
                 print(f"{year} has not been found, skipping")
