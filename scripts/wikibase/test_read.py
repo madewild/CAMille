6158124d1f62c876d795f044f15d91cf50d44cb3
madewild <max.de.wilde@ulb.be>
2024-07-26 13:26:16 +0200

    read test script

---
+++ scripts/wikibase/test_read.py
@@ -0,0 +1,24 @@
+"""Reading triples from SPARQuLb"""
+
+from SPARQLWrapper import SPARQLWrapper, JSON
+import pywikibot
+
+#connect to the wikibase
+wikibase = pywikibot.Site("en", "sparqulb")
+wikibase_repo = wikibase.data_repository()
+wikibase_repo.login()
+
+sparql = SPARQLWrapper("https://query.sparq.ulb.be/bigdata/namespace/wdq/sparql")
+
+query = "select ?s ?o where {?s rdfs:label ?o}"
+
+sparql.setQuery(query)
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+print(results)
+
+bindings = results['results']['bindings']
+print(f"{len(bindings)} items found")
+for result in bindings:
+    qid = result['item']['value'].replace("https://sparq.ulb.be/entity/", "")
+    print(qid)
