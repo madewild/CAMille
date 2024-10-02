fe3bfbf5eb73edbf01c6f330d1fa784ce648da04
madewild <max.de.wilde@ulb.be>
2024-10-02 10:13:15 +0200

    switch to pandas and create list

--- scripts/wikibase/xlsx2json.py
+++ scripts/wikibase/xlsx2json.py
@@ -3,20 +3,23 @@
 import json
 import sys
 
-from openpyxl import load_workbook
+import pandas as pd
 
 PATH = "data/BDD-final2024_bon_juillet31.xlsx"
-wb = load_workbook(filename = PATH)
-sheet = wb['BDD final2024_bon_juillet31']
+df = pd.read_excel(PATH)
 
-line_nb = sys.argv[1]
-data = {}
+limit = int(sys.argv[1])
+datalist = []
 
-data["last_name"] = sheet[f'B{line_nb}'].value
-data["first_name"] = sheet[f'C{line_nb}'].value
-data["full_name"] = " ".join([data["first_name"], data["last_name"]])
-data["country"] = sheet[f'D{line_nb}'].value
-data["gender"] = sheet[f'E{line_nb}'].value
+for line_nb in range(limit):
+    data = {}
+    data["last_name"] = df.iloc[line_nb, 1]
+    data["first_name"] = df.iloc[line_nb, 2]
+    data["full_name"] = " ".join([data["first_name"], data["last_name"]])
+    data["country"] = df.iloc[line_nb, 3]
+    data["gender"] = df.iloc[line_nb, 4]
+    if data not in datalist:
+        datalist.append(data)
 
 with open("data/sample.json", "w", encoding="utf-8") as outfile: 
-    json.dump(data, outfile)
+    json.dump(datalist, outfile, ensure_ascii=False)
