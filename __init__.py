96e9e95ed65b6d8c4b71a7ce756698dcf4612d36
madewild <max.de.wilde@ulb.be>
2025-10-16 14:37:26 +0200

    fix xlsx

--- __init__.py
+++ __init__.py
@@ -288,46 +288,38 @@ def hello():
             df = pd.DataFrame([], columns=['ID', 'JOURNAL', 'DATE', 'ANNÉE', 'MOIS', 'JOUR',
                                             'JDLS', 'ÉDITION', 'PAGE', 'LANGUE', 'TEXTE'])
             for i in range(pages_xlsx):
-                data_page2 =  {
-                    "from": i * 1000,
-                    "size": 1000,
-                    "sort": sort,
-                    "query": query_dic,
-                    "highlight": {
-                        "fields": {
-                            "text": {}
-                        },
-                        "pre_tags": "<kw>",
-                        "post_tags": "</kw>",
-                        "fragment_size": 2000
-                    }
+                highlight = {
+                    "fields": {
+                        "text": {}
+                    },
+                    "pre_tags": "<kw>",
+                    "post_tags": "</kw>",
+                    "fragment_size": 2000
                 }
-                r2 = requests.post(es_url, auth=(username, password), headers=headers,
-                                data=json.dumps(data_page2), timeout=600)
-                if r2.status_code == 200:
-                    resdic2 = json.loads(r2.text)
-                    hits2 = resdic2["hits"]
-                    for hit in hits2["hits"]:
-                        result_id = hit["_source"]["page"]
-                        journal = hit["_source"]["journal"]
-                        date = hit["_source"]["date"]
-                        year = hit["_source"]["year"]
-                        month = hit["_source"]["month"]
-                        day = hit["_source"]["day"]
-                        dow = hit["_source"]["dow"]
-                        edition = hit["_source"]["edition"]
-                        pagenb = hit["_source"]["pagenb"]
-                        language = hit["_source"]["language"]
-                        try:
-                            matches = hit["highlight"]["text"]
-                        except KeyError: # no matches (wildcard), defaulting to 2000 first chars
-                            matches = [hit["_source"]["text"][:2000] + "..."]
-                        text = " [...] ".join(matches)
-                        line = [result_id, journal, date, year, month, day,
-                                dow, edition, pagenb, language, text]
-                        series = pd.Series(line, index=df.columns)
-                        df = df.append(series, ignore_index=True)
-                        #df = pd.concat([df, series], ignore_index=True)
+
+                resdic2 = es.search(index="pages", from_=i*1000, size=1000, sort=sort, query=query_dic, highlight=highlight)
+                hits2 = resdic2["hits"]
+                for hit in hits2["hits"]:
+                    result_id = hit["_source"]["page"]
+                    journal = hit["_source"]["journal"]
+                    date = hit["_source"]["date"]
+                    year = hit["_source"]["year"]
+                    month = hit["_source"]["month"]
+                    day = hit["_source"]["day"]
+                    dow = hit["_source"]["dow"]
+                    edition = hit["_source"]["edition"]
+                    pagenb = hit["_source"]["pagenb"]
+                    language = hit["_source"]["language"]
+                    try:
+                        matches = hit["highlight"]["text"]
+                    except KeyError: # no matches (wildcard), defaulting to 2000 first chars
+                        matches = [hit["_source"]["text"][:2000] + "..."]
+                    text = " [...] ".join(matches)
+                    line = [result_id, journal, date, year, month, day,
+                            dow, edition, pagenb, language, text]
+                    series = pd.Series(line, index=df.columns)
+                    df = df.append(series, ignore_index=True)
+                    #df = pd.concat([df, series], ignore_index=True)
 
             df['DATE'] = pd.to_datetime(df['DATE']).dt.date
             df = df.astype({'ANNÉE': 'int32', 'MOIS': 'int32', 'JOUR': 'int32',
