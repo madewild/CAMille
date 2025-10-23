7c022f0be3c739c995762b9a275b7afa3afeed43
madewild <max.de.wilde@ulb.be>
2025-10-23 19:04:05 +0200

    reenable htpasswd

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ import sys
 from zipfile import ZipFile
 
 from flask import Flask, request, render_template, send_file, redirect
-#from flask_htpasswd import HtPasswdAuth
+from flask_htpasswd import HtPasswdAuth
 
 import pandas as pd
 from unidecode import unidecode
@@ -29,9 +29,9 @@ locale.setlocale(locale.LC_ALL, 'fr_BE.utf8')
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 # comment next 3 lines to disable htpasswd (e.g. if CAS is enabled)
-#app.config['FLASK_HTPASSWD_PATH'] = '/etc/apache2/.htpasswd'
-#app.config['FLASK_AUTH_ALL'] = True
-#htpasswd = HtPasswdAuth(app)
+app.config['FLASK_HTPASSWD_PATH'] = '/etc/apache2/.htpasswd'
+app.config['FLASK_AUTH_ALL'] = True
+htpasswd = HtPasswdAuth(app)
 
 @app.template_filter()
 def strip_param(long_url, param):
