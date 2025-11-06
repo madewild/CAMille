23de460fc48dfb7d0da2028c02f081a4e869bbe6
madewild <max.de.wilde@ulb.be>
2025-11-06 14:52:59 +0100

    look in current dir

--- __init__.py
+++ __init__.py
@@ -33,7 +33,7 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 #app.config['FLASK_AUTH_ALL'] = True
 #htpasswd = HtPasswdAuth(app)
 app.config.update({
-    'OIDC_CLIENT_SECRETS': 'client_secrets.json',
+    'OIDC_CLIENT_SECRETS': './client_secrets.json',
     'OIDC_SCOPES': ['openid', 'email', 'profile'],
     'OIDC_RESOURCE_SERVER_ONLY': False
 })
